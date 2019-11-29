7254cf37f58f62bb8ce4850fcf3a704bff973dda
KrishBaisoya-codingMaster <57061528+KrishBaisoya-codingMaster@users.noreply.github.com>
2019-11-29 15:25:10 +0530

    Added A BAsic Structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { MainComponent } from './Components/Main/main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+ { path: 'Home', component: HomeComponent },
+ { path: 'Main', component: MainComponent },
+
+ { path: 'About', component: AboutComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './Components/nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { MainComponent } from './Components/Main/main/main.component';
+import { App1Component } from './Components/Main/app1/app1.component';
+import { App2Component } from './Components/Main/app2/app2.component';
+import { App3Component } from './Components/Main/app3/app3.component';
+import { Four0fourComponent } from './Components/Error/four0four/four0four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    MainComponent,
+    App1Component,
+    App2Component,
+    App3Component,
+    Four0fourComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TabsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
